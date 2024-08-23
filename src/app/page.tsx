6cd30e2103acce2ef1ae06881f9bfa26728830a4
Arcaxio <jasperkueh@gmail.com>
2024-08-24 01:06:37 +0800

    Kafka V2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,6 +8,7 @@ const fs = require('fs')
 
 const tenantId = '6ecfd23a-e229-4ce1-bf6d-5ebb92e5b8d8';
 const deviceId = '24a00c96-7650-48d8-b8e9-8334e427b757';
+const device2Id = '05642c84-06e0-4e69-84bc-a6439797a3ed';
 const commandTopic = `hono.command.${tenantId}`;
 const eventTopic = `hono.event.${tenantId}`;
 
@@ -30,12 +31,76 @@ const kafka = new Kafka({
 const producer = kafka.producer({ createPartitioner: Partitioners.LegacyPartitioner })
 const consumer = kafka.consumer({ groupId: 'test-group' })
 
+// Runs
+const run = async () => {
+  // Producing an initial message
+  await producer.connect()
+  await producer.send({
+    topic: commandTopic,
+    messages: [
+      { 
+        key: deviceId,
+        value: 'ON',
+        headers: {
+          device_id: deviceId,
+          subject: 'setLight',
+        },
+      }
+    ],
+  })
+  // await producer.send({
+  //   topic: eventTopic,
+  //   messages: [
+  //     { 
+  //       key: device2Id,
+  //       value: '{"lights": "ON"}',
+  //       // headers: {
+  //       //   device_id: deviceId,
+  //       //   subject: 'setLight',
+  //       // },
+  //     }
+  //   ],
+  // })
+  
+  // Consuming messages
+  await consumer.connect()
+  await consumer.subscribe({ topics: [commandTopic, eventTopic] })
+  
+  await consumer.run({
+    eachMessage: async ({ topic, partition, message } : { topic: string, partition: string, message: Message }) => {
+      const messageValue = message.value ? message.value.toString() : null;
+      
+      console.log({
+        topic,
+        partition,
+        offset: message.offset,
+        value: messageValue,
+      });
+  
+      if (messageValue) {
+        await handleMessage(topic, messageValue);
+      } else {
+        console.warn(`Received a message with null value at offset ${message.offset}`);
+      }
+    },
+  })
+}
+
+run().catch(console.error)
+
 // Function to handle the message logic
 const handleMessage = async (topic: string, messageValue: string) => {
   if (topic === eventTopic) {
-    const parsedValue = JSON.parse(messageValue);
+    let parsedValue;
 
-    if (parsedValue.lights === 'OFF') {
+    try {
+      parsedValue = JSON.parse(messageValue);
+    } catch (error) {
+      console.error(`Failed to parse JSON:`);
+      return; // Exit the function if parsing fails
+    }
+
+    if (parsedValue && parsedValue.lights === 'OFF') {
       await producer.send({
         topic: commandTopic,
         messages: [
@@ -49,7 +114,7 @@ const handleMessage = async (topic: string, messageValue: string) => {
           }
         ],
       });
-    } else if (parsedValue.lights === 'ON') {
+    } else if (parsedValue && parsedValue.lights === 'ON') {
       await producer.send({
         topic: commandTopic,
         messages: [
@@ -67,44 +132,6 @@ const handleMessage = async (topic: string, messageValue: string) => {
   }
 }
 
-// Runs
-const run = async () => {
-  // Producing an initial message
-  await producer.connect()
-  await producer.send({
-    topic: commandTopic,
-    messages: [
-      { 
-        key: deviceId,
-        value: 'OFF',
-        headers: {
-          device_id: deviceId,
-          subject: 'setLight',
-        },
-      }
-    ],
-  })
-
-  // Consuming messages
-  await consumer.connect()
-  await consumer.subscribe({ topics: [commandTopic, eventTopic] })
-
-  await consumer.run({
-    eachMessage: async ({ topic, partition, message } : { topic: string, partition: string, message: Message }) => {
-      const messageValue = message.value.toString();
-      console.log({
-        topic,
-        partition,
-        offset: message.offset,
-        value: messageValue,
-      });
-
-      await handleMessage(topic, messageValue);
-    },
-  })
-}
-
-run().catch(console.error)
 
 export default function Home() {
   return (
